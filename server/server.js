19adace173e0f89ef009ce801608d4b12813197c
Rohan Gupta <rohan18100@gmail.com>
2025-12-14 13:39:18 +0530

    server.js updated for vercel

--- server/server.js
+++ server/server.js
@@ -1,11 +1,11 @@
-import express from 'express';
+import express from "express";
 import "dotenv/config";
-import cors from 'cors';
-import http from 'http';
-import { connectDB } from './lib/db.js';
-import userRouter from './routes/userRoutes.js';
-import messageRouter from './routes/messageRoutes.js';
-import {Server} from 'socket.io';
+import cors from "cors";
+import http from "http";
+import { connectDB } from "./lib/db.js";
+import userRouter from "./routes/userRoutes.js";
+import messageRouter from "./routes/messageRoutes.js";
+import { Server } from "socket.io";
 
 // Create Express app and HTTP server
 
@@ -14,60 +14,64 @@ const server = http.createServer(app);
 
 // Initialize Socket.io server
 export const io = new Server(server, {
-    cors: {origin: "*"}
+  cors: { origin: "*" },
 });
 
 // Store online users
-export const userSocketMap={};   //{userId:[socketId1, socketId2, ...]}
+export const userSocketMap = {}; //{userId:[socketId1, socketId2, ...]}
 
 // Socket.io connection handler
 io.on("connection", (socket) => {
-    const userId=socket.handshake.query.userId;
-    console.log("User connected:", userId);
+  const userId = socket.handshake.query.userId;
+  console.log("User connected:", userId);
 
-    if(userId){
-        if (!userSocketMap[userId]) {
-            userSocketMap[userId] = [];
-        }
-        userSocketMap[userId].push(socket.id);
+  if (userId) {
+    if (!userSocketMap[userId]) {
+      userSocketMap[userId] = [];
     }
-    // Emit online users to all connected clients
+    userSocketMap[userId].push(socket.id);
+  }
+  // Emit online users to all connected clients
+  io.emit("getOnlineUsers", Object.keys(userSocketMap));
+
+  // Handle disconnection
+  socket.on("disconnect", () => {
+    console.log("User disconnected:", userId);
+    if (userSocketMap[userId]) {
+      userSocketMap[userId] = userSocketMap[userId].filter(
+        (id) => id !== socket.id
+      );
+      if (userSocketMap[userId].length === 0) {
+        delete userSocketMap[userId];
+      }
+    }
     io.emit("getOnlineUsers", Object.keys(userSocketMap));
-
-    // Handle disconnection
-    socket.on("disconnect", () => {
-        console.log("User disconnected:", userId);
-        if (userSocketMap[userId]) {
-            userSocketMap[userId] = userSocketMap[userId].filter(id => id !== socket.id);
-            if (userSocketMap[userId].length === 0) {
-                delete userSocketMap[userId];
-            }
-        }
-        io.emit("getOnlineUsers", Object.keys(userSocketMap));
-    });
+  });
 });
-
 
 // Middleware
 app.use(cors());
-app.use(express.json({limit: '4mb'}));
-
+app.use(express.json({ limit: "4mb" }));
 
 // Routes
 app.use("/api/status", (req, res) => {
-    res.send("Server is live");
+  res.send("Server is live");
 });
 
 app.use("/api/auth", userRouter);
 
 app.use("/api/messages", messageRouter);
 
-
 // Connect to Database
- await connectDB();
+await connectDB();
 
 // Start server
-const PORT = process.env.PORT || 5000;
-server.listen(PORT, () => {
+if (process.env.NODE_ENV !== "production") {
+  const PORT = process.env.PORT || 5000;
+  server.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
-});
+  });
+}
+
+// export server for Vercel
+export default server;
